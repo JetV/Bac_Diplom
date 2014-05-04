a15dd7c02c73a0a38c767398d464f5c3f1d9cba8
Sergey Kiselev <kiselvserg@gmail.com>
2014-05-04 22:13:39 +0400

    Поменял mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,18 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QLibrary>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked()));
-
-    QLibrary library("C:\\Qt\\sinus\\sincalc-build\\Release\\release\\sincalc.dll");
+    QLibrary library("C:\\Users\\Sergey\\Desktop\\qtproject\\sincalc-build\\Release\\release\\sincalc.dll");
     this->getFun = (_dll_func)library.resolve("SinCalc");
-
-
 }
 
 MainWindow::~MainWindow()
@@ -20,15 +15,32 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::runThread(double arg, _dll_func func)
+{
+    worker_ = new CTWorker(arg, func, &result_);
+    thread_ = new QThread;
+    worker_->moveToThread(thread_);
+
+    connect(thread_, SIGNAL(started()), worker_, SLOT(process()));
+    connect(worker_, SIGNAL(finished()), thread_, SLOT(quit()));
+    connect(worker_, SIGNAL(finished()), this, SLOT(ready()));
+    connect(thread_, SIGNAL(finished()), thread_, SLOT(deleteLater()));
+    thread_->start();
+}
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    on_pushButton_clicked();
+}
+
+void MainWindow::ready()
+{
+    ui->lineEdit_2->setText(QString::number(result_));
+    worker_->deleteLater();
+}
 
 void MainWindow::on_pushButton_clicked()
 {
-
-
-    double operand = this->ui->lineEdit->text().toDouble();
-
-    double result = this->getFun(operand);
-
-    this->ui->lineEdit_2->setText(QString::number(result));
-
+    double arg = this->ui->lineEdit->text().toDouble();
+    runThread(arg, getFun);
 }
